e16e051d59c06e50294211e2440a138e21d528d6
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2025-04-15 15:19:28 +0900

    [level 2] Title: 가장 큰 수, Time: 245.30 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -6,11 +6,9 @@ def solution(numbers):
     # [priority, priority2, value]
     
     for i in range(len(numbers)):
-        priority = int((str(numbers[i]) * 4)[:4])
+        priority = int((str(numbers[i]) * 3)[:3])
         numbers[i] = [priority, str(numbers[i])]
     numbers.sort()
-    # "1111111101011010001000"
-    # "1111111101011010010000"
 
     for i in range(len(numbers)-1,-1,-1):
         if (answer == "0" and numbers[i][0] == 0):
