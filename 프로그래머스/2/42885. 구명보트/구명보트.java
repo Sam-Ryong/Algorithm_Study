4abfb8f97dc49981558016eae7e1f532560e97ca
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2025-06-24 17:03:01 +0900

    [level 2] Title: 구명보트, Time: 11.53 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int low = 0;
+        int high = people.length - 1;
+        
+        while (low <= high){
+            if (people[low] + people[high] <= limit){
+                low++;
+            }
+            high--;
+            answer++;
+        }
+        
+        return answer;
+    }
+    
+    
+}
