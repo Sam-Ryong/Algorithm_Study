f444590e10598f51c1e1fcd15b418920f618b989
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2025-08-12 15:19:00 +0900

    [level 2] Title: 주식가격, Time: 55.07 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,19 +4,21 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        for (int i = 0; i < prices.length - 1; i++){
-            int index = -1;
-            for (int j = i+1; j < prices.length; j++){
-                if (prices[i] > prices[j]){
-                    index = j;
-                    break;
-                }
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < prices.length; i++){
+            
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
             }
-            if (index < 0)
-                answer[i] = prices.length - i - 1;
-            else{
-                answer[i] = index - i;
-            }
+            
+            stack.push(i);
+            
+        }
+        
+        while (!stack.isEmpty()){
+            answer[stack.peek()] = prices.length - 1 - stack.pop();
             
         }
         
