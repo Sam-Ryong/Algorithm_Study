e8e6e53364487132c231b7b96467b3c2a347d84b
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2025-09-15 12:07:29 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 98.37 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -3,40 +3,37 @@ import java.lang.*;
 class Solution {
     public long solution(int[] sequence) {
         long answer = 0;
-        int[] s1 = new int[sequence.length];
-        int[] s2 = new int[sequence.length];
+        int N = sequence.length;
+        long[][] DP = new long[N][2];
         
-        for (int i = 0; i < sequence.length; i++){
+        DP[0][1] = sequence[0];
+        
+        for (int i = 1; i < N; i++){
+            int op = 1;
+            if (i % 2 == 1)
+                op = -1;
             
-            s1[i] = sequence[i];
-            s2[i] = sequence[i];
+            DP[i][0] = Math.max(DP[i-1][0], DP[i-1][1]);
+            
+            DP[i][1] = Math.max(DP[i-1][1] + sequence[i] * op, sequence[i] * op);
+        }
+        
+        answer = Math.max(DP[N-1][0], DP[N-1][1]);
+        
+        DP = new long[N][2];
+        DP[0][1] = -sequence[0];
+        for (int i = 1; i < N; i++){
+            int op = 1;
             if (i % 2 == 0)
-                s1[i] = s1[i] * -1;
-            else
-                s2[i] = s2[i] * -1;
-        }
-        
-        long[][] DP = new long[s1.length][2];
-        DP[0][0] = 0;
-        DP[0][1] = s1[0];
-        
-        for (int i = 1; i < sequence.length; i++){       
+                op = -1;
+            
             DP[i][0] = Math.max(DP[i-1][0], DP[i-1][1]);
-            DP[i][1] = Math.max(DP[i-1][1] + s1[i], s1[i]);   
+            
+            DP[i][1] = Math.max(DP[i-1][1] + sequence[i] * op, sequence[i] * op);
         }
         
-        answer = Math.max(DP[sequence.length - 1][0], (DP[sequence.length - 1][1]));
+        answer = Math.max(answer, Math.max(DP[N-1][0], DP[N-1][1]));
         
-        DP[0][0] = 0;
-        DP[0][1] = s2[0];
-        
-        for (int i = 1; i < sequence.length; i++){       
-            DP[i][0] = Math.max(DP[i-1][0], DP[i-1][1]);
-            DP[i][1] = Math.max(DP[i-1][1] + s2[i], s2[i]);   
-        }
-        
-        answer = Math.max(answer, Math.max(DP[sequence.length - 1][0], (DP[sequence.length - 1][1])));
-                          
         return answer;
     }
 }
