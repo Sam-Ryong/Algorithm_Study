e576fd2c7fbdb38feedf171a63f8567f7dd41b57
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2025-09-16 10:01:23 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 58.35 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,76 +1,72 @@
 import java.lang.*;
-import java.util.*;
 
-class Solution {
+public class Solution {
+
     int answer = Integer.MAX_VALUE;
-    int[][] board;
-    int[][][] DP;
+    int[][] boards;
+    int[][][] visit;
     
-    public int solution(int[][] b) {
+	public int solution(int[][] b) {
+	
+        boards = b;
         
-        board = b;
-        DP = new int[board.length][board.length][2];
+        visit = new int[boards.length][boards.length][2];
+        dfs(1,0,100,"수평");
+        if (visit[boards.length-1][boards.length-1][0] > 0)
+            answer = Math.min(visit[boards.length-1][boards.length-1][0], answer);
+        if (visit[boards.length-1][boards.length-1][1] > 0)
+            answer = Math.min(visit[boards.length-1][boards.length-1][1], answer);
         
-        for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board.length; j++){
-                
-                DP[i][j][0] = Integer.MAX_VALUE;
-                DP[i][j][1] = Integer.MAX_VALUE;
-                
-            }
+        visit = new int[boards.length][boards.length][2];
+        dfs(0,1,100,"수직");
+        if (visit[boards.length-1][boards.length-1][0] > 0)
+            answer = Math.min(visit[boards.length-1][boards.length-1][0], answer);
+        if (visit[boards.length-1][boards.length-1][1] > 0)
+            answer = Math.min(visit[boards.length-1][boards.length-1][1], answer);
+
+        
+        return answer;
+
+	}
+    
+    void dfs(int i, int j, int cost, String direction){
+        
+        if (i > visit.length - 1 || j > visit.length - 1 || i < 0 || j < 0)
+            return;
+        
+        if (boards[i][j] == 1)
+            return;
+  
+        if (direction.equals("수직")){
+            if (visit[i][j][0] == 0 || visit[i][j][0] > cost)
+                visit[i][j][0] = cost;
+            else
+                return;  
+        }
+        else{
+            if (visit[i][j][1] == 0 || visit[i][j][1] > cost)
+                visit[i][j][1] = cost;
+            else
+                return; 
+        }
+
+         
+
+        if (direction.equals("수평")){
+            dfs(i+1,j,cost+100,"수평");
+            dfs(i-1,j,cost+100,"수평");
+            dfs(i,j+1,cost+600,"수직");
+            dfs(i,j-1,cost+600,"수직");
         }
         
-        dfs(0,0,0,"h");
-        dfs(0,0,0,"v");
-        
-        
-        return Math.min(DP[board.length-1][board.length-1][0], DP[board.length-1][board.length-1][1]);
-    }
-    
-    void dfs(int i, int j, int cost, String dir){
-        
-        if (i > board.length - 1 || j > board.length - 1)
-            return;
-        if (i < 0 || j < 0)
-            return;
-        
-        if (board[i][j] == 1)
-            return;
-        
-        if (dir.equals("v") && DP[i][j][0] > cost){             
-            DP[i][j][0] = cost;
-            if (dir.equals("v")){
-                dfs(i+1,j,cost+100,"v");
-                dfs(i-1,j,cost+100,"v");
-                dfs(i,j+1,cost+600,"h");
-                dfs(i,j-1,cost+600,"h");
-            }
-            else if (dir.equals("h")){
-                dfs(i+1,j,cost+600,"v");
-                dfs(i-1,j,cost+600,"v");
-                dfs(i,j+1,cost+100,"h");
-                dfs(i,j-1,cost+100,"h");
-            }
-        }  
-
-        if (dir.equals("h") && DP[i][j][1] > cost){             
-            DP[i][j][1] = cost;
-            if (dir.equals("v")){
-                dfs(i+1,j,cost+100,"v");
-                dfs(i-1,j,cost+100,"v");
-                dfs(i,j+1,cost+600,"h");
-                dfs(i,j-1,cost+600,"h");
-            }
-            else if (dir.equals("h")){
-                dfs(i+1,j,cost+600,"v");
-                dfs(i-1,j,cost+600,"v");
-                dfs(i,j+1,cost+100,"h");
-                dfs(i,j-1,cost+100,"h");
-            }
-        }  
-
+        else {
+            dfs(i+1,j,cost+600,"수평");
+            dfs(i-1,j,cost+600,"수평");
+            dfs(i,j+1,cost+100,"수직");
+            dfs(i,j-1,cost+100,"수직");
+        }
+            
         
     }
-    
-    
+
 }
