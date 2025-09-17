feb7d39e1d2c5e0c516620422434945f19935e21
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2025-09-17 11:09:38 +0900

    [level 3] Title: 인사고과, Time: 178.12 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,50 +2,52 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] t) {
-        int answer = 1;
-       
+        int answer = -1;
         
-        Comparator<Integer[]> cmp = (a,b) -> {
-            if (!a[0].equals(b[0]))
-                return Integer.compare(b[0], a[0]);
-            else{
+        int[] target = new int[]{t[0][0], t[0][1]};
+        List<Integer[]> list = new ArrayList<>();
+        
+        Comparator<int[]> cmp = (a, b) ->{
+            if (a[0] != b[0])
+                return Integer.compare(b[0],a[0]);
+            else
                 return Integer.compare(a[1], b[1]);
-            }
+                
         };
         
-        Integer[][] scores = new Integer[t.length][3];
-        
-        
-        for (int i = 0; i < t.length; i++){
-            scores[i][0] = t[i][0];
-            scores[i][1] = t[i][1];
-            scores[i][2] = i;
-        }
-        
-        Arrays.sort(scores, cmp);
-        int max = -1;
-        for (int i = 0; i < scores.length; i++){
-            
-            if (max > scores[i][1]){
-                if (scores[i][2] == 0){
-                    return -1;
-                }
-                scores[i][2] = -1;
+        Arrays.sort(t, cmp);
+        list.add(new Integer[]{t[0][0], t[0][1]});
+        int maxSecond = t[0][1];
+        for (int i = 1; i < t.length; i++){
+            if (t[i][1] < maxSecond){
+                continue;
             }
             else{
-                max = scores[i][1];
+                list.add(new Integer[]{t[i][0], t[i][1]});
+                maxSecond = t[i][1];
             }
+                
         }
-
         
-        for (int i = 0; i < scores.length; i++){
-            if (scores[i][2] != -1){
-                if (scores[i][0] + scores[i][1] > t[0][0] + t[0][1])
-                    answer++;
-            }
-        }
+        Collections.sort(list, (b, a) -> Integer.compare(a[0] + a[1], b[0] + b[1]));
+        
+        
+        int rank = 1;
+        int count = 1;
 
+        for (Integer[] score : list){
+            if (target[0] + target[1] == score[0] + score[1]){
+                if (target[0] == score[0] && target[1] == score[1]){
+                    answer = rank;
+                    break;
+                }
+            }
+            else {
+                rank++;
+            }
   
+        }
+        
         return answer;
     }
 }
