58e3229f3b0f53c6f3aab39f93a5a1a03b6c13d0
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2025-09-17 10:06:12 +0900

    [level 3] Title: 거스름돈, Time: 83.53 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,27 +1,36 @@
+import java.lang.*;
+
 class Solution {
     public int solution(int n, int[] money) {
-        int answer = 0;
+    
+        int[][] DP = new int[n + 1][money.length + 1];
         
-        int[][] DP = new int[n+1][money.length+1];
-        
-        for (int i = 0; i < money.length; i++){
-            DP[money[i]][i+1] = 1;
+        for (int i = 1; i < DP[0].length; i++){
+            
+            DP[0][i] = 1;
+            
         }
-        
-        for (int i = 1; i < n+1; i++){
-            for (int j = 1; j < money.length+1; j++){
-                
-                DP[i][j] += DP[i][j-1];
 
-                if (i - money[j-1] >= 0){
-                    DP[i][j] += DP[i-money[j-1]][j];     
+        for (int i = 1; i < DP.length; i++){
+            
+            for (int j = 1; j < DP[0].length; j++){
+                
+                int coin = money[j-1];
+                
+                if (i - coin >= 0){
+                    DP[i][j] = (DP[i-coin][j] + DP[i][j-1]) % 1000000007;
                 }
-      
+                else{
+                    DP[i][j] = DP[i][j-1];
+                }
+                
             }
+            
         }
         
+
         
-        
-        return DP[n][money.length];
+        return DP[n][money.length] ;
+    
     }
 }
